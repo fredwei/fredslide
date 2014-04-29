54f8ec3d6f25066ae070ea4485912ad394ad1273
fredwei <250095176@qq.com>
2014-04-29 17:33:44 +0800

    v1.0.0
    第一版

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+	var a1 = $('#slide2').fredslide({
+		SwitchStyle : 'toleft',
+		AutoPlay : true
+	});
+
+	var a2 = $('#slide').fredslide({
+		SwitchStyle : 'fade',
+		AutoPlay : true
+	});
+
+	console.log(a1);
+	console.log(a2);
+});
